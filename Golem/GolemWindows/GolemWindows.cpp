af86cae873d9b500c412990e45f4f7f050dc2ebf
whuop <brodal@landfall.se>
2020-05-28 10:32:27 +0200

    bgfx included

--- Golem/GolemWindows/GolemWindows.cpp
+++ Golem/GolemWindows/GolemWindows.cpp
@@ -3,29 +3,25 @@
 #include "SDL.h"
 #undef main
 #include <iostream>
+#include <bgfx/bgfx.h>
+#include <bgfx/platform.h>
+#include <bx/bx.h>
+
 #include "Window.h"
+#include "Renderer.h"
 
 using namespace Golem::Core;
 
 int main()
 {
 	Window* window = new Window(100, 100, 640, 480);
+	Renderer* renderer = new Renderer(window);
 
 	window->Run();
 
+	delete renderer;
+	renderer = nullptr;
 	delete window;
 	window = nullptr;
-
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
